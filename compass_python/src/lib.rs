124a86bef988b77c126b3f1412d06d413f7978e2
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-10-28 20:42:34 -0400

    Boiler Plate Code

--- compass_python/src/lib.rs
+++ compass_python/src/lib.rs
@@ -1,6 +1,11 @@
 use pyo3::prelude::*;
 
+use pyo3_stub_gen::define_stub_info_gatherer;
+
+use pyo3_stub_gen::derive::gen_stub_pyfunction;
+
 /// Formats the sum of two numbers as string.
+#[gen_stub_pyfunction(module = "ospl_rcompass._rust_lib")]
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
@@ -8,7 +13,10 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn compass_python(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _rust_lib(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
+
+// Define a function to gather stub information.
+define_stub_info_gatherer!(stub_info);
---
+++ compass_python/src_python/ospl_rcompass/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+A Rust backend library aiming to provide high performance
+implementations for OpenSpeleo project.
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("ospl_rcompass")
