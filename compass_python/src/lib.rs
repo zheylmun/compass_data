fa706fc2e61cfc1f916328cf547b7cd8d4cf9409
Zach <zheylmun@gmail.com>
2025-10-29 11:25:07 -0400

    Get py03 binding and strongly typed inputs for conversion function

--- compass_python/src/lib.rs
+++ compass_python/src/lib.rs
@@ -1,40 +1,79 @@
-use pyo3::prelude::*;
-use pyo3::types::PyBytes;
-use pyo3::types::PyDict;
-use pyo3::types::PyModule;
+use pyo3::{prelude::*, types::PyBytes};
+use pyo3_stub_gen::{
+    define_stub_info_gatherer,
+    derive::{gen_stub_pyclass, gen_stub_pyfunction},
+};
+use serde::{Deserialize, Serialize};
 
-use pyo3_stub_gen::define_stub_info_gatherer;
-use pyo3_stub_gen::derive::gen_stub_pyfunction;
+#[pyclass]
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+enum Units {
+    Feet,
+    Meters,
+}
+
+#[pyclass]
+#[gen_stub_pyclass]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Shot {
+    from: String,
+    to: String,
+    length: f32,
+    azimuth: f32,
+    depth: f32,
+    left: Option<f32>,
+    right: Option<f32>,
+    up: Option<f32>,
+    down: Option<f32>,
+    flags: String,
+    comment: String,
+}
+
+#[pyclass]
+#[gen_stub_pyclass]
+#[derive(Clone, Debug)]
+struct SurveyData {
+    survey_date: String,
+    units: Units,
+    cave_name: String,
+    survey_name: String,
+    survey_team: String,
+    comment: String,
+    latitude: f32,
+    longitude: f32,
+    shots: Vec<Shot>,
+}
 
 /// A Python-exposed function that returns fixed DAT file bytes.
 #[gen_stub_pyfunction(module = "ospl_rcompass._rust_lib")]
 #[pyfunction]
 fn convert_xls_json_to_dat<'py>(
     py: Python<'py>,
-    data: &Bound<'py, PyDict>,
+    data: SurveyData,
 ) -> PyResult<Bound<'py, PyBytes>> {
-    let mut bytes = br#"Fulford Cave
-SURVEY NAME: SS
-SURVEY DATE: 8 28 1988  COMMENT:Surface to shelter
-SURVEY TEAM:
-Mike Roberts,Ken Kreager,Rick Rhinehart, ,
-DECLINATION:   11.18  FORMAT: DDDDUDLRLADN  CORRECTIONS:  0.00 0.00 0.00
+    // Figure out the magnetic declination based on the survey date and location
+    let parameters = compass_data::Parameters {
+        declination: 0.0,
+        correction_factors: None,
+        backsight_correction_factors: None,
+    };
+    let date = chrono::NaiveDate::parse_from_str(&data.survey_date, "%Y-%m-%d").unwrap();
+    let comment = match data.comment.is_empty() {
+        true => None,
+        false => Some(data.comment),
+    };
 
-        FROM           TO   LENGTH  BEARING      INC     LEFT       UP     DOWN    RIGHT   FLAGS  COMMENTS
-
-          A1          SS1    62.45   104.00    34.50 -9999.00 -9999.00 -9999.00 -9999.00  #|P#
-         SS1          SS2    35.35   120.50    22.00 -9999.00 -9999.00 -9999.00 -9999.00  #|P#
-         SS2          SS3    25.35   150.50    10.50 -9999.00 -9999.00 -9999.00 -9999.00  #|P#
-         SS3          SS4    67.20   117.00    29.50 -9999.00 -9999.00 -9999.00 -9999.00  #|P#
-         SS4          SS5    60.10   123.50    16.00 -9999.00 -9999.00 -9999.00 -9999.00  #|P#
-         SS5          SS6    54.50   112.00    11.00 -9999.00 -9999.00 -9999.00 -9999.00  #|P#
-         SS6          SS7    36.30    89.00    21.00 -9999.00 -9999.00 -9999.00 -9999.00
-         SS6          SS8    41.70   333.50    -2.50 -9999.00 -9999.00 -9999.00 -9999.00
-"#.to_vec();
-
-    // Append a literal form-feed (0x0C)
-    bytes.push(0x0C);
-
+    let compass_survey = compass_data::Survey {
+        cave_name: data.cave_name,
+        name: data.survey_name,
+        date,
+        team: data.survey_team,
+        comment,
+        parameters,
+        shots: vec![],
+    };
+    let serialized = compass_survey.serialize();
+    let bytes = serialized.as_bytes();
     // Return a PyBytes object (Bound<'py, PyBytes>)
     Ok(PyBytes::new(py, &bytes))
 }
@@ -42,6 +81,9 @@ DECLINATION:   11.18  FORMAT: DDDDUDLRLADN  CORRECTIONS:  0.00 0.00 0.00
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _rust_lib(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Units>()?;
+    m.add_class::<Shot>()?;
+    m.add_class::<SurveyData>()?;
     m.add_function(wrap_pyfunction!(convert_xls_json_to_dat, m)?)?;
     Ok(())
 }
