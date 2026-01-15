0f628a316d83bc02df6b852312593ee631b83505
Zach Heylmun <zheylmun@gmail.com>
2026-01-15 10:14:52 -0500

    Add test that runs additional projects from an environment variable driven directory

--- src/lib.rs
+++ src/lib.rs
@@ -15,16 +15,37 @@ mod tests {
     use project::Project;
 
     use super::*;
-    use std::path::PathBuf;
+    use std::path::{Path, PathBuf};
+
+    fn test_at_path(project_path: &Path) {
+        let unloaded_project = Project::read(&project_path).unwrap();
+        // Make sure we can load all the project files too
+        let _loaded_project = unloaded_project.load_survey_files().unwrap();
+    }
 
     #[test]
     fn parse_compass_sample() {
         let mut sample_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         sample_path.push("test_data/Fulfords.mak");
-
         let unloaded_project = Project::read(&sample_path).unwrap();
         assert_eq!(unloaded_project.survey_files.len(), 2);
         // Make sure we can load all the project files too
         let _loaded_project = unloaded_project.load_survey_files().unwrap();
+    }
+
+    #[test]
+    fn parse_environment_samples() {
+        if let Ok(sample_dir) = std::env::var("COMPASS_DATA_SAMPLES") {
+            let sample_dir = std::fs::read_dir(sample_dir).unwrap();
+            for file_entry in sample_dir {
+                let file_path = file_entry.unwrap().path();
+                if file_path.exists()
+                    && file_path.is_file()
+                    && file_path.extension().unwrap().to_str() == Some("mak")
+                {
+                    test_at_path(&file_path);
+                }
+            }
+        }
     }
 }
